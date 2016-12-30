3e6de6f580f4a3ba244c7b3a18da32250aa17cf7
David Wang <david.wang34@gmail.com>
2016-12-29 19:49:17 -0500

    visual changes

--- SimpleSky/HourlyViewController.swift
+++ SimpleSky/HourlyViewController.swift
@@ -8,55 +8,31 @@
 
 import UIKit
 
-class HourlyViewController: UIViewController, WeatherGetterDelegate {
+class HourlyViewController: UIViewController {
     
     @IBOutlet weak var cityLabel: UILabel!
     
     var weather: WeatherGetter!
-
-    //let weatherObject = UserDefaults.standard.object(forKey: "weather") as? Weather
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        print(weather)
+        /*
+ 
+ let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
+ let blurEffectView = UIVisualEffectView(effect: blurEffect)
+ blurEffectView.frame = view.bounds
+ blurEffectView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+ view.addSubview(blurEffectView)
+ 
+ */
         
     }
     
-    func didGetWeather(weather: Weather) {
-        
-        self.cityLabel.text = weather.city
-        
-        
-        
-        //DispatchQueue.main.asynchronously() {
-        //self.cityLabel.text = weather.city
-        /*self.weatherLabel.text = weather.weatherDescription
-         self.temperatureLabel.text = "\(Int(round(weather.tempCelsius)))°"
-         self.cloudCoverLabel.text = "\(weather.cloudCover)%"
-         self.windLabel.text = "\(weather.windSpeed) m/s"
-         
-         if let rain = weather.rainfallInLast3Hours {
-         self.rainLabel.text = "\(rain) mm"
-         } else {
-         self.rainLabel.text = "None"
-         }
-         
-         self.humidityLabel.text = "\(weather.humidity)%"*/
-        //}
-    }
-    
-    func didNotGetWeather(error: NSError) {
-        // This method is called asynchronously, which means it won't execute in the main queue.
-        // ALl UI code needs to execute in the main queue, which is why we're wrapping the call
-        // to showSimpleAlert(title:message:) in a dispatch_async() call.
-        //DispatchQueue.main.asynchronously() {
-        /*self.showSimpleAlert(title: "Can't get the weather",
-         message: "The weather service isn't responding.")*/
-        //}
-        print("didNotGetWeather error: \(error)")
-    }
+    /*override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = .clearColor()
+    }*/
 
 
     /*
